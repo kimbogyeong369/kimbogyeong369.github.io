cbe003454166c050a6cf394e1470760c3b4ce688
kimbogyeong369 <ruvy369@gmail.com>
2024-07-21 21:01:02 +0900

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const customCursor = document.querySelector('.custom-cursor');
+
+// 마우스 움직임 이벤트 리스너
+document.addEventListener('mousemove', (e) => {
+    // 커서 위치를 마우스 위치로 설정
+    customCursor.style.left = e.pageX + 'px';
+    customCursor.style.top = e.pageY + 'px';
+});
+
+// 마우스 호버 이벤트 리스너
+document.addEventListener('mouseenter', () => {
+    customCursor.style.display = 'block'; // 커서 보이기
+});
+
+document.addEventListener('mouseleave', () => {
+    customCursor.style.display = 'none'; // 커서 숨기기
+});
+
+
+
+
