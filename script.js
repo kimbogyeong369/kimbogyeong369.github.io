ac0f3c0f573974005043f2776c5f8db2d5b47848
kimbogyeong369 <ruvy369@gmail.com>
2024-08-08 00:41:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ document.addEventListener('mousemove', (e) => {
     customCursor.style.top = e.pageY + 'px';
 });
 
-// 마우스 호버 이벤트 리스너
-document.addEventListener('mouseenter', () => {
-    customCursor.style.display = 'block'; // 커서 보이기
-});
-
-document.addEventListener('mouseleave', () => {
-    customCursor.style.display = 'none'; // 커서 숨기기
-});
-
 // 페이지 로드 시 커서 숨기기
 window.addEventListener('load', () => {
     customCursor.style.display = 'none'; // 초기 상태에서 커서 숨기기
